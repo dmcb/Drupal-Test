952cb280b91957e3a2f6287a4b4e49c0a443f7a1
dmcb <d.mcburney@gmail.com>
2015-06-02 22:42:25 -0600

    Following Drush naming conventions

--- run.sh
+++ run.sh
@@ -13,11 +13,11 @@ ROOT="$4"
 URI="$5"
 
 export BEHAT_PARAMS="{\"extensions\":{\"Behat\\\\MinkExtension\":{\"base_url\":\"$BASE_URL\"}}}"
-cp behat_aliases.drushrc.php.default behat_aliases.drushrc.php
-sed -i "s!\$REMOTE_HOST!$REMOTE_HOST!" behat_aliases.drushrc.php
-sed -i "s!\$REMOTE_USER!$REMOTE_USER!" behat_aliases.drushrc.php
-sed -i "s!\$ROOT!$ROOT!" behat_aliases.drushrc.php
-sed -i "s!\$URI!$URI!" behat_aliases.drushrc.php
-cp behat_aliases.drushrc.php ~/.drush/behat_aliases.drushrc.php
+cp behat.aliases.drushrc.php.default behat.aliases.drushrc.php
+sed -i "s!\$REMOTE_HOST!$REMOTE_HOST!" behat.aliases.drushrc.php
+sed -i "s!\$REMOTE_USER!$REMOTE_USER!" behat.aliases.drushrc.php
+sed -i "s!\$ROOT!$ROOT!" behat.aliases.drushrc.php
+sed -i "s!\$URI!$URI!" behat.aliases.drushrc.php
+cp behat.aliases.drushrc.php ~/.drush/behat.aliases.drushrc.php
 
 bin/behat

2032dbbe77b8a7ab61a386f43e1cd538993b816b
dmcb <d.mcburney@gmail.com>
2015-06-22 15:50:39 -0600

    Create compatibility with OS X sed

--- run.sh
+++ run.sh
@@ -14,10 +14,10 @@ URI="$5"
 
 export BEHAT_PARAMS="{\"extensions\":{\"Behat\\\\MinkExtension\":{\"base_url\":\"$BASE_URL\"}}}"
 cp behat.aliases.drushrc.php.default behat.aliases.drushrc.php
-sed -i "s!\$REMOTE_HOST!$REMOTE_HOST!" behat.aliases.drushrc.php
-sed -i "s!\$REMOTE_USER!$REMOTE_USER!" behat.aliases.drushrc.php
-sed -i "s!\$ROOT!$ROOT!" behat.aliases.drushrc.php
-sed -i "s!\$URI!$URI!" behat.aliases.drushrc.php
+sed -i -e "s!\$REMOTE_HOST!$REMOTE_HOST!" behat.aliases.drushrc.php
+sed -i -e "s!\$REMOTE_USER!$REMOTE_USER!" behat.aliases.drushrc.php
+sed -i -e "s!\$ROOT!$ROOT!" behat.aliases.drushrc.php
+sed -i -e "s!\$URI!$URI!" behat.aliases.drushrc.php
 mkdir -p ~/.drush
 cp behat.aliases.drushrc.php ~/.drush/behat.aliases.drushrc.php
 
